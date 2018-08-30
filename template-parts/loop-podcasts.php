83de01c1a0c2576b76ed0f2f665a1340f8ee5937
Daniel Vena <deaconvena+github@gmail.com>
2018-08-30 15:23:57 -0400

    genre pod cards without images now have titles

--- template-parts/loop-podcasts.php
+++ template-parts/loop-podcasts.php
@@ -15,6 +15,12 @@ namespace WWOPN_Theme;
 						<figure>
 								<?php \the_post_thumbnail() ?>
 						</figure>
+					<?php else: ?>
+						<div class="headline">
+							<h2 class="stext st_blue" data-st-src="<?=\get_template_directory_uri()?>/assets/prod/images/stext/right.svg">
+								<?php \the_title() ?>
+							</h2>
+						</div>
 					<?php endif ?>
 
 					<div class="content">
