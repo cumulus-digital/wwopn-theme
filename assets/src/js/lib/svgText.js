8cee23b4b7f5da38d025e167ecc1f7b6fb225f9b
Daniel Vena <deaconvena+github@gmail.com>
2018-09-01 02:29:07 -0400

    more kerning pairs, changed data attrs to be shorter

--- assets/src/js/lib/svgText.js
+++ assets/src/js/lib/svgText.js
@@ -158,8 +158,8 @@
 
 					var xmlns = "http://www.w3.org/2000/svg",
 						svg = me.createElNS(xmlns, 'svg');
-					svg.setAttribute('data-letter', letter);
-					svg.setAttribute('data-width', dimensions[3]);
+					svg.setAttribute('data-l', letter);
+					svg.setAttribute('data-w', dimensions[3]);
 					svg.setAttributeNS(
 						"http://www.w3.org/2000/xmlns/",
 						"xmlns:xlink",
