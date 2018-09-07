15d93d60f71dfbcb4f3b74320863fbe4c553f1e4
Daniel Vena <deaconvena+github@gmail.com>
2018-09-07 17:15:53 -0400

    display social and store links with noopener

--- single-wpn_podcast.php
+++ single-wpn_podcast.php
@@ -120,7 +120,7 @@ namespace WWOPN_Theme;
 								<?php foreach(\WWOPN_Podcast\CPT::getSocialLinks() as $soc_service=>$soc_link): ?>
 									<?php if ($soc_link): ?>
 									<li class="\esc_attr($soc_service) ?>">
-										<a href="<?=\esc_url($soc_link) ?>">
+										<a href="<?=\esc_url($soc_link) ?>" target="_blank" rel="noopener">
 											<?php if ($soc_service=='facebook'): ?>
 												<svg class="facebook" aria-labelledby="facebook-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M22.676 0H1.324C.593 0 0 .593 0 1.324v21.352C0 23.408.593 24 1.324 24h11.494v-9.294H9.689v-3.621h3.129V8.41c0-3.099 1.894-4.785 4.659-4.785 1.325 0 2.464.097 2.796.141v3.24h-1.921c-1.5 0-1.792.721-1.792 1.771v2.311h3.584l-.465 3.63H16.56V24h6.115c.733 0 1.325-.592 1.325-1.324V1.324C24 .593 23.408 0 22.676 0"/></svg>
 												<span>
@@ -168,7 +168,7 @@ namespace WWOPN_Theme;
 
 								<?php if ($storelink): ?>
 								<li class="<?=\esc_attr($store) ?>">
-									<a href="<?=\esc_url($storelink)?>" target="_blank">
+									<a href="<?=\esc_url($storelink)?>" target="_blank" rel="noopener">
 										<img src="<?=\get_template_directory_uri()?>/assets/prod/images/badges/<?=\esc_attr($store) ?>.svg" alt="listen on <?=\esc_attr($store) ?>">
 									</a>
 								</li>
